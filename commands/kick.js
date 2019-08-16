4aad99eb743ffcfbeacd8cf80ac079895b981639
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-08-16 15:36:23 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,13 +3,13 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     message.delete()
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("[Asuma] : L'utilisateur est introuvable. :exclamation:");
+    if(!kUser) return message.channel.send("[69sex] : L'utilisateur est introuvable. :exclamation:");
     let kReason = args.join(" ").slice(22);
-    if(!kReason) return message.channel.send("[Asuma] : Vous devez inscrire la raison du kick. :exclamation:");
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("[Asuma] : Vous n'avez pas la permission. :exclamation:");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("[Asuma] : Cet utilisateur ne peut pas être kick. :exclamation:");
-    kUser.send(`[Asuma] : Vous êtes kick pour : ${kReason}. :spy:`).catch(err => console.log(err))
-    message.channel.send(`[Asuma] : L'utilisateur ${kUser} vient de se faire kick pour : ${kReason}. :exclamation:`)
+    if(!kReason) return message.channel.send("[69sex] : Vous devez inscrire la raison du kick. :exclamation:");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("[69sex] : Vous n'avez pas la permission. :exclamation:");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("[69sex] : Cet utilisateur ne peut pas être kick. :exclamation:");
+    kUser.send(`[69sex] : Vous êtes kick pour : ${kReason}. :spy:`).catch(err => console.log(err))
+    message.channel.send(`[69sex] : L'utilisateur ${kUser} vient de se faire kick pour : ${kReason}. :exclamation:`)
 
     let kickEmbed = new Discord.RichEmbed()
     .setDescription("Kick")
@@ -21,8 +21,8 @@ module.exports.run = async (bot, message, args) => {
     .addField("Dâte :", message.createdAt.toLocaleString())
     .addField("Raison :", kReason);
 
-    let kickChannel = message.guild.channels.find(`name`, "asuma-logs");
-    if(!kickChannel) return message.channel.send("[Asuma] : Le channel #Asuma-logs est introuvable. :exclamation:");
+    let kickChannel = message.guild.channels.find(`name`, "69sex-logs");
+    if(!kickChannel) return message.channel.send("[69sex] : Le channel #69sex-logs est introuvable. :exclamation:");
 
     message.guild.member(kUser).kick(kReason);
     kickChannel.send(kickEmbed);
