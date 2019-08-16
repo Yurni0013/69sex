c1e25df0df6280e292e355cba78a28e592f4df22
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-08-16 15:34:26 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Vous n'avez pas la permission misérable! :exclamation:");
-  if(!args[0]) return message.channel.send("[Asuma] Vous devez inscrire un nombre de messages à supprimer. :exclamation:");
+  if(!args[0]) return message.channel.send("[69SEX] Vous devez inscrire un nombre de messages à supprimer. :exclamation:");
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`[Asuma] ${args[0]} message(s) ont été supprimé.:exclamation:`).then(msg => msg.delete(5000));
+    message.channel.send(`[69SEX] ${args[0]} message(s) ont été supprimé.:exclamation:`).then(msg => msg.delete(5000));
   });
 }
 
