9db9aa201abe47be767d7a8254dc9d5dd9895e32
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-08-18 14:02:39 +0200

    Update msgsond.js

--- commands/msgsond.js
+++ commands/msgsond.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
-    .setTitle("**Sondage n°1**")
+    .setTitle("**Sondage n°2**")
     .setColor("#000000")
     .setThumbnail('https://i.imgur.com/3u1z5DT.gif')
-    .addField('**__Vous êtes :__**', '🚬 : **Fumeur** ou :no_smoking: **Non fumeur**', true)
+    .addField('**__Vous préférez :__**', ':calling:  **Android** ou :green_apple:  **iOS**', true)
     .setFooter('69Sex💋', 'https://i.imgur.com/3u1z5DT.gif');
     message.channel.send(botembed);
 }
