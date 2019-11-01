99c3c0e269e9b68b14a6098a416b4b9fce4f5529
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-11-01 19:27:13 +0100

    Update msgemb.js

--- commands/msgemb.js
+++ commands/msgemb.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
-    .setTitle("Bienvenue sur le server discord de la **Hebi**")
+    .setTitle("Bienvenue sur le server discord de **Habbot Hotel**")
     .setColor("#000000")
     .setAuthor("✔️ | Vérification")
-    .setThumbnail('https://i.imgur.com/XnoZbaW.gif')
+    .setThumbnail('https://i.imgur.com/CkMhVlK.png')
     .addField("Ce salon à été crée afin de protéger le serveur contre les attaques et les account ROBOT, pour accéder à l'intégralité du serveur et prouver que tu n'es pas un robot.","Il te reste plus qu'une chose à faire, cliquer sur la réaction se situant sur le message ci-dessous! Ce après quoi tu pourras accéder au serveur !")
-    .setFooter('Habbot🗻', 'https://i.imgur.com/K7C1Rxi.gif');
+    .setFooter('Habbot🗻', 'https://i.imgur.com/CkMhVlK.png');
     message.channel.send(botembed);
 }
 
