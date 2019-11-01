affdefaf954e66e473a027cc836d6ce6f5bb1e9a
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-11-01 18:52:36 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setAuthor('Asuma', 'https://i.imgur.com/gtBOpRE.jpg', 'https://discord.gg/KUNvmNu')
-    .setThumbnail('https://i.imgur.com/gtBOpRE.jpg')
+    .setAuthor('Habbot', 'https://i.imgur.com/CkMhVlK.png', 'https://discord.gg/HKYmxF5')
+    .setThumbnail('https://i.imgur.com/CkMhVlK.png')
     .setColor("#EE82EE")
-    .addField('👕 | Inviter Asuma sur votre serveur:', '[Clique ici](https://discordapp.com/oauth2/authorize?client_id=550960584364130304&scope=bot&permissions=8)', true)
+    .addField('👕 | Inviter Habbot BOT sur votre serveur:', '[Clique ici](https://discordapp.com/oauth2/authorize?client_id=550960584364130304&scope=bot&permissions=8)', true)
     .setTimestamp()
-    .setFooter('Nistype', 'https://i.imgur.com/gtBOpRE.jpg');
+    .setFooter('Habbot🗻', 'https://i.imgur.com/CkMhVlK.png');
 
     message.channel.send(botembed);
 }
