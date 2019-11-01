41473bb61db11f9a41d802e3ebd10863c4d5dc96
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-11-01 18:49:24 +0100

    Update botmessage.js

--- commands/botmessage.js
+++ commands/botmessage.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("[69SEX] Vous n'avez pas la permission misérable! :exclamation:");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("[HABBOT] Vous n'avez pas la permission misérable! :exclamation:");
   let botmessage = args.join(" ");
   message.delete().catch();
   message.channel.send(botmessage);
