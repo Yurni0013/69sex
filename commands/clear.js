dc049d8e445882d22b1cf7c638efb16737689767
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-11-01 18:50:56 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Vous n'avez pas la permission misérable! :exclamation:");
   if(!args[0]) return message.channel.send("[69SEX] Vous devez inscrire un nombre de messages à supprimer. :exclamation:");
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`[69SEX] ${args[0]} message(s) ont été supprimé.:exclamation:`).then(msg => msg.delete(5000));
+    message.channel.send(`[HABBOT] ${args[0]} message(s) ont été supprimé.:exclamation:`).then(msg => msg.delete(5000));
   });
 }
 
