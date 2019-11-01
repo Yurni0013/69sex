990108bd41049f5272729968db02510080e4769d
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-11-01 19:26:37 +0100

    Create msgemb.js

---
+++ commands/msgemb.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let botembed = new Discord.RichEmbed()
+    .setTitle("Bienvenue sur le server discord de la **Hebi**")
+    .setColor("#000000")
+    .setAuthor("✔️ | Vérification")
+    .setThumbnail('https://i.imgur.com/XnoZbaW.gif')
+    .addField("Ce salon à été crée afin de protéger le serveur contre les attaques et les account ROBOT, pour accéder à l'intégralité du serveur et prouver que tu n'es pas un robot.","Il te reste plus qu'une chose à faire, cliquer sur la réaction se situant sur le message ci-dessous! Ce après quoi tu pourras accéder au serveur !")
+    .setFooter('Habbot🗻', 'https://i.imgur.com/K7C1Rxi.gif');
+    message.channel.send(botembed);
+}
+
+module.exports.help = {
+  name:"msgemb"
+}
