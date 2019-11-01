bb9bc262a7a6721e0846e5340cb06231440cef18
Yurni0013 <52999108+Yurni0013@users.noreply.github.com>
2019-11-01 18:54:51 +0100

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#0099ff")
     .setImage('https://i.imgur.com/u7t8FPb.gif')
     .setTimestamp()
-    .setFooter('69Sex💋', 'https://i.imgur.com/3u1z5DT.gif');
+    .setFooter('Habbot🗻', 'https://i.imgur.com/CkMhVlK.png');
 
     message.channel.send(botembed);
 }
